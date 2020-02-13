479a4ed89c09acb9df08b4dc5d899cdb5fc76732
Paul McManus <pmcmanus2021@gmail.com>
2020-02-13 11:28:08 +0000

    Init Commit

---
+++ src/main/java/com/example/codeclan/SpringCourseTracker/SpringCourseTrackerApplication.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.SpringCourseTracker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCourseTrackerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringCourseTrackerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/codeclan/SpringCourseTracker/SpringCourseTrackerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.codeclan.SpringCourseTracker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCourseTrackerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
