9d34a7793d3ad56276d3ea85f138bcea140b446b
Paul McManus <pmcmanus2021@gmail.com>
2020-02-13 14:06:37 +0000

    AddsCoursesForACustomer

--- src/main/java/com/example/codeclan/SpringCourseTracker/repositories/CourseRepository.java
+++ src/main/java/com/example/codeclan/SpringCourseTracker/repositories/CourseRepository.java
@@ -1,9 +1,15 @@
 package com.example.codeclan.SpringCourseTracker.repositories;
 
 import com.example.codeclan.SpringCourseTracker.models.Course;
+import com.example.codeclan.SpringCourseTracker.models.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface CourseRepository extends JpaRepository<Course, Long> {
+    List<Course> findByRating(int rating);
+
+    List<Course> findByBookingsCustomer(Customer customer);
 }
--- src/test/java/com/example/codeclan/SpringCourseTracker/SpringCourseTrackerApplicationTests.java
+++ src/test/java/com/example/codeclan/SpringCourseTracker/SpringCourseTrackerApplicationTests.java
@@ -10,6 +10,10 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 @SpringBootTest
 class SpringCourseTrackerApplicationTests {
 
@@ -29,7 +33,7 @@ class SpringCourseTrackerApplicationTests {
 	@Test
 	public void canCreateAndSaveAll(){
 
-		Course course = new Course("Needlework","Glasgow",5);
+		Course course = new Course("Needlework","Glasgow",4);
 		courseRepository.save(course);
 
 		Customer customer = new Customer("Gregor","Glasgow",30);
@@ -37,9 +41,31 @@ class SpringCourseTrackerApplicationTests {
 
 		Booking booking = new Booking("13-02-20",course,customer);
 		bookingRepository.save(booking);
+	}
 
+	@Test
+	public void canGetCoursesByRating(){
+		List<Course> foundCourses = courseRepository.findByRating(5);
+		assertEquals(1, foundCourses.size());
+	}
 
+	@Test
+	public void canGetAllCustomersForACourse(){
+		List<Customer> foundCustomers = customerRepository.findByBookingsCourseId(1L);
+		assertEquals(2, foundCustomers.size());
+	}
 
+	@Test
+	public void canGetAllCoursesForACustomer(){
+		Customer stephen = new Customer("Tony","Edinburgh", 10);
+		customerRepository.save(stephen);
+		Course behaviour = new Course("Behaviour", "Edinburgh",2);
+		courseRepository.save(behaviour);
+		Booking booking2 = new Booking("28-02-20", behaviour, stephen);
+		bookingRepository.save(booking2);
+
+		List<Course> foundCourses = courseRepository.findByBookingsCustomer(stephen);
+		assertEquals(1, foundCourses.size());
 	}
 
 }
